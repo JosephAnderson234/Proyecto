9ca9a2aaddbaaa3f6388d3e11928dd774168d259
JosephAnderson234 <joseph.cose@utec.edu.pe>
2024-11-10 08:27:30 -0500

    first changes, created the class of the table

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+class Ctablero {
+    vector<vector<char>> tablas;
+    int size;
+public:
+
+
+};
+
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
