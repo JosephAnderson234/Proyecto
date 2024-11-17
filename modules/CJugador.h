6390857607451a251f22e6bbaf8d7012d5bc32e2
JhoganPa <pachacutecjhogan@gmail.com>
2024-11-17 18:19:48 -0500

    Primera version de la clase CJugador

---
+++ modules/CJugador.h
@@ -0,0 +1,22 @@
+#ifndef CJUGADOR_H
+#define CJUGADOR_H
+#include <string>
+#include <vector>
+using namespace std;
+
+class CJugador {
+    string nombre;
+    int turnos;
+    int dificultad;
+public:
+    CJugador(string name, int turnes, int dificulty): nombre(name), turnos(turnes), dificultad(dificulty) {};
+    friend class CTablero;
+    friend void guardar_estadisticas(CJugador* gamer);
+    friend void mostrarEstadisticas();
+    friend void ordenarNivel(vector <CJugador*> jugadores_dificultad);
+    friend bool compararJugadores(CJugador* a, CJugador* b);
+};
+
+
+
+#endif //CJUGADOR_H
