1c5e3377f62bcbd1de5ef3ff43bae2dbe2006a2d
JhoganPa <pachacutecjhogan@gmail.com>
2024-11-17 18:18:07 -0500

    Create estadisticas.h

---
+++ modules/estadisticas.h
@@ -0,0 +1,19 @@
+#ifndef ESTADISTICAS_H
+#define ESTADISTICAS_H
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "CJugador.h"
+#include "menu.h"
+
+//Actualizar estadisticas por cada jugador nuevo
+void guardar_estadisticas(CJugador* gamer);
+
+//Ordenar y clasificar a los jugadores
+vector <CJugador*> cargar_jugadores();
+bool compararJugadores(CJugador* a, CJugador* b);
+void ordenarNivel(vector <CJugador*> jugadores_dificultad);
+void mostrarEstadisticas();
+
+
+#endif //ESTADISTICAS_H
